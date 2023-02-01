4c0d104715b754cf878725e3b421a74032752da9
vinodcanchipuramkumar <95145546+vinodcanchipuramkumar@users.noreply.github.com>
2023-02-01 10:56:27 +0530

    Add files via upload

---
+++ src/main/db/db-schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS `fithealthdb`;
+use  `fithealthdb`;
+
+CREATE TABLE IF NOT EXISTS `patient` (
+  `patient_no` int NOT NULL AUTO_INCREMENT,
+  `patient_nm` varchar(45) DEFAULT NULL,
+  `mobile_nbr` varchar(45) DEFAULT NULL,
+  `email_address` varchar(45) DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  `gender` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`patient_no`)
+);
